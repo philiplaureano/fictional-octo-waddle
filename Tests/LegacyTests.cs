79f10283479e9f11b87fe761b803f5379272aec0
Philip Laureano <philip.laureano@medicaldirector.com>
2018-09-18 17:06:09 +1000

    Added the first failing test that documents what the method does in one test case

--- Tests/LegacyTests.cs
+++ Tests/LegacyTests.cs
@@ -1,4 +1,5 @@
 using System;
+using LegacyCodeLibrary;
 using Xunit;
 
 namespace Tests
@@ -6,8 +7,15 @@ namespace Tests
     public class LegacyTests
     {
         [Fact]
-        public void Test1()
+        public void ShouldReturnCorrectValueWhenEnteringTestString()
         {
+            var result = Formatter.FormatText("<test/>");
+            
+            // The test will initially fail, but it will give us the value
+            // returned by the legacy component
+            Assert.Equal(null, result);
+            
+            // TODO: Write the resulting assertion here with the expected actual value
         }
     }
 }
