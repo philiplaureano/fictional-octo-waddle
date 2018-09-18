11b540874332aabd9096cc4a8db0b18b69b25ba0
Philip Laureano <philip.laureano@medicaldirector.com>
2018-09-19 09:40:59 +1000

    Added a test that ensures that the FormatText method throws a null ref exception when a null value is passed into the method

--- Tests/LegacyTests.cs
+++ Tests/LegacyTests.cs
@@ -9,26 +9,23 @@ namespace Tests
         [Fact]
         public void ShouldReturnCorrectValueWhenEnteringTestString()
         {
-            var result = Formatter.FormatText("<test/>");           
-            Assert.Equal(string.Empty, result);           
+            var result = Formatter.FormatText("<test/>");
+            Assert.Equal(string.Empty, result);
         }
 
         [Fact]
         public void ShouldReturnCorrectValueWhenEnteringEmptyString()
         {
-            var result = Formatter.FormatText(string.Empty);            
+            var result = Formatter.FormatText(string.Empty);
             Assert.Equal(string.Empty, result);
         }
 
         [Fact]
         public void ShouldBehaveCorrectlyWhenPassingNullValue()
         {
-            var result = Formatter.FormatText(null);
-            
-            // Note: I'm using a random string to guarantee that the test will never pass
-            // and so that it'll give me the actual return value that will be used
-            // as the expected value
-            Assert.Equal(Guid.NewGuid().ToString(), result);
+            // Ensure that the FormatText method throws a null ref exception
+            // when a null is passed
+            Assert.Throws<NullReferenceException>(() => Formatter.FormatText(null));
         }
     }
 }
