b15772ab7284b508264cd922d6b5dfb60fcd8900
Philip Laureano <philip.laureano@medicaldirector.com>
2018-09-19 09:25:18 +1000

    Added a failing test for checking the behavior when passing a null value to the FormatText method

--- Tests/LegacyTests.cs
+++ Tests/LegacyTests.cs
@@ -19,5 +19,16 @@ namespace Tests
             var result = Formatter.FormatText(string.Empty);            
             Assert.Equal(string.Empty, result);
         }
+
+        [Fact]
+        public void ShouldBehaveCorrectlyWhenPassingNullValue()
+        {
+            var result = Formatter.FormatText(null);
+            
+            // Note: I'm using a random string to guarantee that the test will never pass
+            // and so that it'll give me the actual return value that will be used
+            // as the expected value
+            Assert.Equal(Guid.NewGuid().ToString(), result);
+        }
     }
 }
