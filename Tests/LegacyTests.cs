49a956c0ae505d41af6bf64583ea8fcc8cd44de2
Philip Laureano <philip.laureano@medicaldirector.com>
2018-09-18 17:09:39 +1000

    Added the first complete characterization test

--- Tests/LegacyTests.cs
+++ Tests/LegacyTests.cs
@@ -9,13 +9,8 @@ namespace Tests
         [Fact]
         public void ShouldReturnCorrectValueWhenEnteringTestString()
         {
-            var result = Formatter.FormatText("<test/>");
-            
-            // The test will initially fail, but it will give us the value
-            // returned by the legacy component
-            Assert.Equal(null, result);
-            
-            // TODO: Write the resulting assertion here with the expected actual value
+            var result = Formatter.FormatText("<test/>");           
+            Assert.Equal(string.Empty, result);           
         }
     }
 }
