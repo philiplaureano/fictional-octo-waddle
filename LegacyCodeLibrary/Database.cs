49edb25de7833120fef8f79ab55ad3dafa0411d2
Philip Laureano <philip.laureano@medicaldirector.com>
2018-09-27 14:19:39 +1000

    Added sample legacy code that simulates a static call to a database library (for testing purposes)

---
+++ LegacyCodeLibrary/Database.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LegacyCodeLibrary
+{
+    public class Database
+    {
+        public static void Save(string text)
+        {
+            // TODO: Insert some really ugly legacy code here
+            Console.WriteLine($"Saving string '{text}'");
+        }
+    }
+}
--- LegacyCodeLibrary/Formatter.cs
+++ LegacyCodeLibrary/Formatter.cs
@@ -5,24 +5,34 @@ namespace LegacyCodeLibrary
 {
     public class Formatter
     {
-        public static String FormatText(String text)
+        public static String FormatText(string text)
         {
             var stringBuffer = new StringBuilder();
-            
-            for (int n = 0; n < text.Length; ++n) {
+
+            for (int n = 0; n < text.Length; ++n)
+            {
                 int c = text[n];
-                if (c == '<') {
-                    while(n < text.Length && text[n] != '/' && text[n] != '>')
+                if (c == '<')
+                {
+                    while (n < text.Length && text[n] != '/' && text[n] != '>')
                         n++;
                     if (n < text.Length && text[n] == '/')
-                        n+=4;
+                        n += 4;
                     else
                         n++;
                 }
+
                 if (n < text.Length)
                     stringBuffer.Append(text[n]);
             }
+
             return stringBuffer.ToString();
+        }
+
+        public static void SaveText(string text)
+        {
+            var formattedText = FormatText(text);
+            Database.Save(formattedText);
         }
     }
 }
